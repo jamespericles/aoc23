c85bb56de82820e1db838548683db6d856228ee2
James Pericles <jamesspericles@gmail.com>
2023-12-01 15:18:28 -0600

    solved part 1

---
+++ days/01/index.ts
@@ -0,0 +1,43 @@
+// Prompt: https://adventofcode.com/2020/day/1
+
+import { readFileSync } from 'fs' 
+import { join } from 'path'
+
+let input: string[] = readFileSync(join(__dirname, 'input.txt'), { encoding: 'utf-8' }).split('\n')
+
+let sum = 0 
+let inputLine = 1
+
+const wordToDigit = { 
+  'one': '1',
+  'two': '2', 
+  'three': '3', 
+  'four': '4', 
+  'five': '5', 
+  'six': '6', 
+  'seven': '7', 
+  'eight': '8', 
+  'nine': '9', 
+}
+
+for (let line of input) { 
+  let numbers = line.match(/\d|one|two|three|four|five|six|seven|eight|nine/g) || []
+
+for (let word in wordToDigit) { 
+  // @ts-ignore 
+  numbers = numbers.map(num => num.replace(new RegExp(word, 'g'), wordToDigit[word as keyof typeof wordToDigit])) 
+}
+
+let firstNumber = numbers[0] 
+let lastNumber = numbers.length > 1 ? numbers[numbers.length - 1] : firstNumber
+
+if (firstNumber && lastNumber) { 
+  let twoDigitNumber = Number(firstNumber + lastNumber) 
+  console.log(`Line ${inputLine}: ${twoDigitNumber}`)
+
+  sum += twoDigitNumber 
+  inputLine += 1 
+  }
+}
+
+console.log(`Sum: ${sum}`)
