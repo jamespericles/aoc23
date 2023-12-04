e2a633043f5116a9e1a184d8a10595d2301697f9
James Pericles <jamesspericles@gmail.com>
2023-12-04 17:57:45 -0600

    slight rewrite for 4-1, finished 4-2

--- days/04/index.ts
+++ days/04/index.ts
@@ -10,25 +10,23 @@ let part2 = 0
 const cardInstances = new Array(input.length).fill(1)
 
 input.forEach((line, idx) => {
-  let cardValue = 0
   const [, winning, card] = line.split(/[:|]/g)
 
   const winningNumbers = winning.trim().split(/\s+/).map(Number)
   const cardNumbers = card.trim().split(/\s+/).map(Number)
   let matchCount = cardNumbers.filter((num) => winningNumbers.includes(num)).length
 
-  while (matchCount) {
-    if (cardValue === 0) {
-      cardValue++
-      matchCount--
-    } else {
-      cardValue *= 2
-      matchCount--
+  if (matchCount) {
+    const points = 2 ** (matchCount - 1)
+    part1 += points
+
+    for (let i = idx + 1; i <= idx + matchCount; i++) {
+      cardInstances[i] += cardInstances[idx]
     }
   }
-
-  part1 += cardValue
 })
+
+part2 = cardInstances.reduce((acc, curr) => acc + curr, 0)
 
 console.log(`Part 1: ${part1}`)
 console.log(`Part 2: ${part2}`)
